8065e6a678f54a71effe7dafe476f976c7e07eb5
KritinPw <kritin.p@gofive.co.th>
2026-01-06 11:03:29 +0700

    update Button style

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,10 +1,15 @@
-// src/Button.tsx
-import type { ReactNode } from "react";
+'use client';
+
+import React from 'react';
 
 type ButtonProps = {
   children?: React.ReactNode;
 };
 
 export const Button = ({ children }: ButtonProps) => {
-  return <button style={{ color: "red" ,backgroundColor:"blue"}}>{children}</button>;
+  return (
+    <button style={{ color: 'red', backgroundColor: 'blue' }}>
+      {children}
+    </button>
+  );
 };
