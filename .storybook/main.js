fe7c7a7521f7031358edb3808ced3409192b0f7f
KritinPw <kritin.p@gofive.co.th>
2026-01-07 13:34:55 +0700

    set up config for seperate module

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,17 @@
-
-
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  "stories": [
+  stories: [
     "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../projects/**/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
     "@storybook/addon-docs",
     "@storybook/addon-onboarding"
   ],
-  "framework": "@storybook/react-vite"
+  framework: "@storybook/react-vite"
 };
+
 export default config;
---
+++ projects/button/src/index.ts
@@ -0,0 +1,4 @@
+// projects/button/src/index.ts
+
+export * from "./lib/components/button/Button";
+export * from "./lib/components/buttonSplit/ButtonSplit";
